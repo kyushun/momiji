c8f147a8df07d9e1f00013dfa853d1a0be36a235
kyushun <27068326+kyushun@users.noreply.github.com>
2024-01-27 12:58:44 +0900

    fix: import path

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,6 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
 
-if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-  throw new Error("Missing Google OAuth credentials.");
-}
-
-export const authOptions: AuthOptions = {
-  debug: process.env.NODE_ENV !== "production",
-  session: { strategy: "jwt" },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-          scope: [
-            "https://www.googleapis.com/auth/userinfo.profile",
-            "https://www.googleapis.com/auth/userinfo.email",
-            "https://www.googleapis.com/auth/calendar.readonly",
-          ].join(" "),
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    jwt: async ({ token, account }) => {
-      if (account) {
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-      }
-
-      return token;
-    },
-    session: ({ session, token }) => ({
-      ...session,
-      token: {
-        accessToken: token.accessToken,
-        refreshToken: token.refreshToken,
-      },
-    }),
-  },
-};
+import { authOptions } from "@/utility/next-auth";
 
 const handler = NextAuth(authOptions);
 
--- src/utility/oauth2-client.ts
+++ src/utility/oauth2-client.ts
@@ -1,7 +1,7 @@
 import { google } from "googleapis";
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "./next-auth";
 
 export const createOAuth2Client = async () => {
   const session = await getServerSession(authOptions);
